6a5a58bc91c1c8613e17122fcce5b75f44f9f743
Tanveer <orangegamerz2124@gmail.com>
2025-11-29 16:13:08 +0530

    first

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <div className='text-5xl font-black text-center  text-blue-400 p-4 '> IIIT GYMKHANA</div>
+    </>
+  )
+}
+
+export default App
